53c2f759d00496ba545133b7fa3f7b5affc47de3
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-21 22:09:03 +0530

    welcome

---
+++ src/main/java/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,15 @@
+/**
+ * ****************************************************************************
+ * Purpose: This Class Mood Analyzer is implemented to Handle Custom Exceptions,
+ * handle exception if user provides invalid mood, code to take the mood message
+ * in constructor
+ *
+ * @author Dhiraj
+ * @version 1.0
+ * @since 21-06-2021 **********************************************************
+ */
+
+package bridgelabz;
+
+public class MoodAnalyzer {
+}
