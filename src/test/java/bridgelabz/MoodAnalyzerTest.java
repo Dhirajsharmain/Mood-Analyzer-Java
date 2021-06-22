2a932dec0ca3ae6ea409f2390060040e1d02c451
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-22 13:06:59 +0530

    Added : TC-1.1 analyseMood method can just return SAD

--- src/test/java/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/bridgelabz/MoodAnalyzerTest.java
@@ -1,4 +1,18 @@
 package bridgelabz;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MoodAnalyzerTest {
+
+    MoodAnalyzer moodAnalyzer;
+
+    @Test
+    public void givenNull_shouldReturnHappy(){
+        moodAnalyzer = new MoodAnalyzer("SAD");
+        String result = moodAnalyzer.analyseMood();
+        assertEquals("SAD", result);
+
+    }
 }
