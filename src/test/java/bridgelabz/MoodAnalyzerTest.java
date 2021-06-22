1400d9896a4e4dd2c80591e65ccbfcc25b50cca3
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-22 13:12:17 +0530

    Added : TC-1.2 Given, I am in Any Mood,  message Should Return HAPPY

--- src/test/java/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/bridgelabz/MoodAnalyzerTest.java
@@ -9,10 +9,18 @@ public class MoodAnalyzerTest {
     MoodAnalyzer moodAnalyzer;
 
     @Test
-    public void givenNull_shouldReturnHappy(){
+    public void givenSad_shouldReturnSad(){
         moodAnalyzer = new MoodAnalyzer("SAD");
         String result = moodAnalyzer.analyseMood();
         assertEquals("SAD", result);
+
+    }
+
+    @Test
+    public void givenAny_shouldReturnHappy(){
+        moodAnalyzer = new MoodAnalyzer("any");
+        String result = moodAnalyzer.analyseMood();
+        assertEquals("HAPPY", result);
 
     }
 }
